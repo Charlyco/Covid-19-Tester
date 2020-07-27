0b465e38e3c72547870f027a8a9dcc9d35221f9b
Onuoha Chukwuemeka <52838613+Charlyco@users.noreply.github.com>
2020-07-28 00:30:15 +0100

    Used Navigation controller to handle options menu Item selection

--- app/src/main/java/com/example/covid_19tester/MainActivity.java
+++ app/src/main/java/com/example/covid_19tester/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.covid_19tester;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.main_main_activity, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.onNavDestinationSelected(item, navController )
+        || super.onOptionsItemSelected(item);
+    }
 }
